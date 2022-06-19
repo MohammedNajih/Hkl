9f4ab237e2d69ae382d0dc4b43a33455ca8c2826
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2022-06-20 02:21:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ def masg(call):
 		M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
 		
 		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️",reply_markup=mas)
-
 	elif call.data =="F1":
 		
 		mas = types.InlineKeyboardMarkup(row_width=2)
@@ -63,7 +62,8 @@ def masg(call):
 		M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
 		mas.add(C,H,U,G,M)
 		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="- WELCOME TO MY FUCTION INSTAGRAM PLEASE CHOISE ONE FUCTION",reply_markup=mas)
-		
+		if call.data =="F2":
+			bot.send_message(message.chat.id, f"- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️",reply_markup=mas)
 		
 		
 	elif call.data =="F2":
