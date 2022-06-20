bd5b2a3654b0c037d4d236fb2b5ebae1ea3a4cb7
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2022-06-20 03:53:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,63 +12,65 @@ logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
 
+"""
+	C = types.InlineKeyboardButton(text ="CHECKER", callback_data="I1")
+	H = types.InlineKeyboardButton(text ="HUNTER", callback_data="I2")
+	U = types.InlineKeyboardButton(text ="CHECK USER", callback_data="I3")
+	G = types.InlineKeyboardButton(text ="GREAT ACCOUNT", callback_data="I4") 
+"""
 @bot.message_handler(commands=['start'])
 def boten(message):
-	
-    
-    
-    mas = types.InlineKeyboardMarkup(row_width=2)
-    
-    I = types.InlineKeyboardButton(text ="INSTAGRAM", callback_data="F1")
-    
-    F = types.InlineKeyboardButton(text ="FACEBOOK", callback_data="F2")
-    
-    T = types.InlineKeyboardButton(text =" TIKTOK ", callback_data="F3")
-    
-    S = types.InlineKeyboardButton(text ="SNAPCHAT", callback_data="F4")
-    
-    M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
-    
-    mas.add(I,F,T,S,M)
-    
-    bot.send_message(message.chat.id, f"- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️",reply_markup=mas)
-    
-    
+	mas = types.InlineKeyboardMarkup(row_width=2) 
+	I = types.InlineKeyboardButton(text ="INSTAGRAM", callback_data="F1")
+	F = types.InlineKeyboardButton(text ="FACEBOOK", callback_data="F2")
+	T = types.InlineKeyboardButton(text =" TIKTOK ", callback_data="F3")
+	S = types.InlineKeyboardButton(text ="SNAPCHAT", callback_data="F4")
+	M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
+	mas.add(I,F,T,S,M)
+	bot.send_message(message.chat.id, f"- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️",reply_markup=mas)
+
 @bot.callback_query_handler(func=lambda call: True)
 def masg(call):
-	
-	
 	global nam
-	
 	if call.data =="MohammedNajih":
-		
 		mas = types.InlineKeyboardMarkup(row_width=2)
-		
 		I = types.InlineKeyboardButton(text ="INSTAGRAM", callback_data="F1")
 		F = types.InlineKeyboardButton(text ="FACEBOOK", callback_data="F2")
 		T = types.InlineKeyboardButton(text =" TIKTOK ", callback_data="F3")
 		S = types.InlineKeyboardButton(text ="SNAPCHAT", callback_data="F4")
 		M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
-		
 		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️",reply_markup=mas)
 	if call.data =="F1":
-		
 		mas = types.InlineKeyboardMarkup(row_width=2)
-		
 		C = types.InlineKeyboardButton(text ="CHECKER", callback_data="I1")
 		H = types.InlineKeyboardButton(text ="HUNTER", callback_data="I2")
 		U = types.InlineKeyboardButton(text ="CHECK USER", callback_data="I3")
 		G = types.InlineKeyboardButton(text ="GRAT ACCOUNT", callback_data="I4")
 		M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
-		mas.add(C,H,U,G,M)
-		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="- WELCOME TO MY FUCTION INSTAGRAM PLEASE CHOISE ONE FUCTION", reply_markup=mas)
-		if call.data =="I1":
-			bot.send_message(message.chat.id, f"- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️")
+		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="- WELCOME TO MY FUCTION INSTAGRAM PLEASE CHOISE ONE FUCTION",reply_markup=mas)
 		
+@bot.message_handler(commands=['insta'])
+def sms(message):
+	IN = types.InlineKeyboardMarkup(row_width=2) 
+	C = types.InlineKeyboardButton(text ="CHECKER", callback_data="I1")
+	H = types.InlineKeyboardButton(text ="HUNTER", callback_data="I2")
+	U = types.InlineKeyboardButton(text ="CHECK USER", callback_data="I3")
+	G = types.InlineKeyboardButton(text ="GREAT ACCOUNT", callback_data="I4")
+	M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
+	mas.add(C,H,U,G,M)
+	bot.send_message(message.chat.id, f"- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️",reply_markup=IN)
+@bot.callback_query_handler(func=lambda call: True)
+def masg(call):
+	global nam
+	if call.data =="MM":
+		IN = types.InlineKeyboardMarkup(row_width=2)
+		C = types.InlineKeyboardButton(text ="CHECKER", callback_data="I1")
+		H = types.InlineKeyboardButton(text ="HUNTER", callback_data="I2")
+		U = types.InlineKeyboardButton(text ="CHECK USER", callback_data="I3")
+		G = types.InlineKeyboardButton(text ="GREAT ACCOUNT", callback_data="I4") 
+		M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
+		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️",reply_markup=IN)
 		
-	if call.data =="F2":
-		bot.send_message(message.chat.id, f"- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️")
-
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
