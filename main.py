1a1ec62927e3d900091808d895643cbb9d6683b2
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2022-06-20 05:01:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ def boten(message):
 	F = types.InlineKeyboardButton(text ="FACEBOOK", callback_data="F2")
 	T = types.InlineKeyboardButton(text =" TIKTOK ", callback_data="F3")
 	S = types.InlineKeyboardButton(text ="SNAPCHAT", callback_data="F4")
+	W = types.InlineKeyboardButton(text ="Twitter", callback_data="F5")
 	M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
-	mas.add(I,F,T,S,M)
+	mas.add(I,F,T,S,W,M)
 	logo = 'https://firebasestorage.googleapis.com/v0/b/shoteriq.appspot.com/o/IMG_20220527_223323_610.jpg?alt=media&token=20802a14-94fe-481e-a404-aaed52bb7784'
 	bot.send_photo(message.chat.id,logo, f"- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️",reply_markup=mas)
 
@@ -34,6 +35,7 @@ def masg(call):
 		F = types.InlineKeyboardButton(text ="FACEBOOK", callback_data="F2")
 		T = types.InlineKeyboardButton(text =" TIKTOK ", callback_data="F3")
 		S = types.InlineKeyboardButton(text ="SNAPCHAT", callback_data="F4")
+		W = types.InlineKeyboardButton(text ="Twitter", callback_data="F4")
 		M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
 		logo = 'https://firebasestorage.googleapis.com/v0/b/shoteriq.appspot.com/o/IMG_20220527_223323_610.jpg?alt=media&token=20802a14-94fe-481e-a404-aaed52bb7784'
 		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text="- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️",reply_markup=mas)
@@ -49,7 +51,7 @@ def masg(call):
 	if call.data =="F4":
 		sanp = 'https://firebasestorage.googleapis.com/v0/b/shoteriq.appspot.com/o/PicsArt%20MQ_06-20-04.41.34.png?alt=media&token=a8946655-367b-4d41-b6b7-16d5fa9e13b7'
 		bot.send_photo(call.message.chat.id,snap,"Welcome to Menu SnapChat\nSEND /snap IN BOOT" )
-	if call.data =="F2":
+	if call.data =="F5":
 		twi = 'https://firebasestorage.googleapis.com/v0/b/shoteriq.appspot.com/o/PicsArt%20MQ_06-20-04.41.03.png?alt=media&token=e3825d71-2b1a-45ac-8e76-4cf33f6a34d4'
 		bot.send_photo(call.message.chat.id,twi,"Welcome to Menu Twitter\nSEND /twi IN BOOT" )
 @bot.message_handler(commands=['insta'])
