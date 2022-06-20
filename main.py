b5a675ee030f215eb75ee4b76bf77cd8d776723e
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2022-06-20 06:25:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,6 @@ def boten(message):
 	I = types.InlineKeyboardButton(text ="ABOUT", callback_data="about")
 	M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
 	mas.add(I,M)
-	logo = 'https://firebasestorage.googleapis.com/v0/b/shoteriq.appspot.com/o/IMG_20220527_223323_610.jpg?alt=media&token=20802a14-94fe-481e-a404-aaed52bb7784'
 	bot.send_message(message.chat.id, f"- ️",reply_markup=mas)
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -75,8 +74,7 @@ def masg(call):
 		logo = 'https://firebasestorage.googleapis.com/v0/b/shoteriq.appspot.com/o/IMG_20220527_223323_610.jpg?alt=media&token=20802a14-94fe-481e-a404-aaed52bb7784'
 		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text="- ",reply_markup=mas)
 	if call.data =="about":
-		instagram = 'https://firebasestorage.googleapis.com/v0/b/shoteriq.appspot.com/o/PicsArt%20MQ_06-20-04.37.10.png?alt=media&token=cdcb997c-a027-40c9-8951-e381984d8a4b'
-		bot.send_photo(call.message.chat.id,instagram,"مرحبا بك في قائمة الانستا اختر الان احد الخيارات؛ \n 1 لبدأ فحص الحسابات قم بارسال /checkinsta في البوت \n 2 لبدأ فحص المتاحان قم بارسال /huntinsta في البوت \n 3 لبدأ فحص اليوزرات قم بارسال /userinsta في البوت \n 4 لبدأ انشاء الحسابات قم بارسال /ginsta في البوت" )
+		bot.send_photo(call.message.chat.id,instagram,f"مرحبا بك في قائمة الانستا اختر الان احد الخيارات؛ \n 1 لبدأ فحص الحسابات قم بارسال /checkinsta في البوت \n 2 لبدأ فحص المتاحان قم بارسال /huntinsta في البوت \n 3 لبدأ فحص اليوزرات قم بارسال /userinsta في البوت \n 4 لبدأ انشاء الحسابات قم بارسال /ginsta في البوت" )
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
