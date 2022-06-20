c7bef64b048b4761d61f875968f922ca7c73018a
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2022-06-20 04:12:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ def masg(call):
 		M = types.InlineKeyboardButton('DEVELOPER', url='https://t.me/ONCLIK')
 		bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️",reply_markup=mas)
 	if call.data =="F1":
-		bot.send_message(message.chat.id, f"- WELCOME TO MY BOT PLEASE CHOISE ANY FUCTION️")
+			pr = 'https://firebasestorage.googleapis.com/v0/b/shoteriq.appspot.com/o/IMG_20220527_223323_610.jpg?alt=media&token=20802a14-94fe-481e-a404-aaed52bb7784' 
+			bot.send_photo(message.chat.id,pr,f"$_____MOHAMMED ALMUSWI IS BACK____$")
 		
 @bot.message_handler(commands=['insta'])
 def sms(message):
