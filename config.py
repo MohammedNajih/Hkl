2cc5344091176465d18d82f07ab534e26c4ae3c9
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2022-12-13 06:41:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 BOT_TOKEN = "5734388256:AAFh-F14Tw-lqpoJC0df6ilUYVWqKzLZeNA"
-APP_URL = "https://mohammed-almuswi.herokuapp.com/" + BOT_TOKEN
+APP_URL = "https://mohammed-almuswi.onrender.com/" + BOT_TOKEN
